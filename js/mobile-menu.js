137be1ffba214537df3e2166475ac0651227639d
Benjamin Nneji <nnejiben@gmail.com>
2023-07-20 23:36:49 +0100

    new music player
    Changed to music player by SamirPaul

---
+++ js/mobile-menu.js
@@ -0,0 +1,14 @@
+// MOBILE MENU
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".mobile-nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".mobile-nav-link").forEach(
+    n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
---
+++ js/music-list.js
@@ -0,0 +1,42 @@
+
+// img must be in .jpg format
+// src must be in .mp3 format
+
+let allMusic = [
+    {
+      name: "Harley Bird - Home",
+      artist: "Jordan Schor",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Ikson Anywhere - Ikson",
+      artist: "Audio Library",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+        name: "A Thousand Miles",
+        artist: "Vanessa Carlton",
+        img: "",
+        src: "a-thousand-miles"
+      }
+  ];
