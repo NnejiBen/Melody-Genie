923956bcc56e3e1273b830f59a1e27c5af5ee6e1
Benjamin Nneji <nnejiben@gmail.com>
2023-07-21 11:30:33 +0100

    home music player complete

--- js/music-list.js
+++ js/music-list.js
@@ -4,51 +4,76 @@
 
 let allMusic = [
     {
-      name: "Harley Bird - Home",
-      artist: "Jordan Schor",
-      img: "music-1",
-      src: "music-1"
+      name: "A Thousand Miles",
+      artist: "Vanessa Carlton",
+      img: "music-01",
+      src: "music-01"
     },
     {
-      name: "Ikson Anywhere - Ikson",
-      artist: "Audio Library",
-      img: "music-2",
-      src: "music-2"
+      name: "Ordinary Day",
+      artist: "Vanessa Carlton",
+      img: "music-02",
+      src: "music-02"
     },
     {
-      name: "Beauz & Jvna - Crazy",
-      artist: "Beauz & Jvna",
-      img: "music-3",
-      src: "music-3"
+      name: "Pretty Baby",
+      artist: "Vanessa Carlton",
+      img: "music-03",
+      src: "music-03"
     },
     {
-      name: "Hardwind - Want Me",
-      artist: "Mike Archangelo",
-      img: "music-4",
-      src: "music-4"
+      name: "Breakaway",
+      artist: "Kelly Clarkson",
+      img: "music-04",
+      src: "music-04"
     },
     {
-      name: "Jim - Sun Goes Down",
-      artist: "Jim Yosef x Roy",
-      img: "music-5",
-      src: "music-5"
+      name: "Complicated",
+      artist: "Avril Lavigne",
+      img: "music-05",
+      src: "music-05"
     },
     {
-        name: "A Thousand Miles",
-        artist: "Vanessa Carlton",
-        img: "",
-        src: "a-thousand-miles"
+        name: "I'm With You",
+        artist: "Avril Lavigne",
+        img: "music-06",
+        src: "music-06"
       },
       {
-        name: "Ordinary Day",
-        artist: "Vanessa Carlton",
-        img: "",
-        src: "ordinary-day"
+        name: "Nine Million Bicycles",
+        artist: "Katie Melua",
+        img: "music-07",
+        src: "music-07"
       },
       {
-        name: "Pretty Baby",
-        artist: "Vanessa Carlton",
-        img: "",
-        src: "pretty-baby"
+        name: "She Will Be Loved",
+        artist: "Maroon 5",
+        img: "music-08",
+        src: "music-08"
+      },
+      {
+        name: "Unwritten",
+        artist: "Natasha Bedingfield",
+        img: "music-09",
+        src: "music-09"
+      },
+      {
+        name: "When Will I See You Again?",
+        artist: "The Three Degrees",
+        img: "music-10",
+        src: "music-10"
       }
   ];
+
+
+// PLAYLIST
+// music-01 - A Thousand Miles - Vanessa Carlton
+// music-02 - Ordinary Day - Vanessa Carlton
+// music-03 - Pretty Baby - Vanessa Carlton
+// music-04 - Breakaway - Kelly Clarkson
+// music-05 - Complicated - Avril Lavigne
+// music-06 - I'm With You - Avril Lavigne
+// music-07 - Nine Million Bicycles - Katie Melua
+// music-08 - She Will Be Loved - Maroon 5
+// music-09 - Unwritten - Natasha Bedingfield
+// music-10 - When Will I See You Again? - The Three Degrees
