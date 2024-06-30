2b0ed4f028d28d5d8551635569dcc436d0b06813
Benjamin Nneji <nnejiben@gmail.com>
2024-06-30 01:23:03 +0100

    create song-lyrics.js

---
+++ js/song-lyrics.js
@@ -0,0 +1,17 @@
+// Variables
+const lyrics01 = document.getElementById("lyrics-01");
+const lyrics01Ext01 = document.getElementById("lyrics-01-ext-01");
+const lyrics01Ext02 = document.getElementById("lyrics-01-ext-02");
+
+const lyrics02 = document.getElementById("lyrics-02");
+const lyrics02Ext01 = document.getElementById("lyrics-02-ext-01");
+const lyrics02Ext02 = document.getElementById("lyrics-02-ext-02");
+const lyrics02Ext03 = document.getElementById("lyrics-02-ext-03");
+
+const lyrics01MouseOver = () => {
+    lyrics01Ext02.style.display = "block";
+    lyrics02Ext01.style.display = "none";
+    lyrics02Ext02.style.display = "none";
+}
+
+lyrics01.addEventListener("mouseover", lyrics01MouseOver);
